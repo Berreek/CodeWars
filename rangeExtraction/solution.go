8a24fa4ea9436fc1b50e5226f7a2a2ab177a0eca
Bartosz Behring <bartosz@spotio.com>
2021-08-29 18:32:15 +0200

    another one

---
+++ rangeExtraction/solution.go
@@ -0,0 +1,14 @@
+package rangeExtraction
+
+import "sort"
+
+func Solution(list []int) string {
+	sort.Ints(list)
+
+	result :=
+	for _, i := range list {
+
+	}
+
+	return ""
+}
---
+++ rangeExtraction/solution_test.go
@@ -0,0 +1,1 @@
+package rangeExtraction_test
